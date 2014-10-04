1097592600f2d9878d3dd747a51906c166b14507
Glavin Wiechert <glavin.wiechert@gmail.com>
2014-10-04 07:51:08 -0400

    Invites are sent to both users in connection.

--- testProject/api/models/Connection.js
+++ testProject/api/models/Connection.js
@@ -19,8 +19,37 @@ module.exports = {
     }
   },
 
-  afterCreate: function(connection, callback) {
+  sendInvite: function(user1, user2, callback) {
 
+    var myAccessToken = user1.linkedinAccessToken;
+    var linkedin = Linkedin.init(myAccessToken);
+
+    linkedin.people.invite({
+      "recipients": {
+        "values": [{
+          "person": {
+            "_path": "/people/email=" + user2.emailAddress,
+            "first-name": user2.firstName,
+            "last-name": user2.lastName
+          }
+        }]
+      },
+      "subject": "Invitation to connect.",
+      "body": "We met with Mymi",
+      "item-content": {
+        "invitation-request": {
+          "connect-type": "friend"
+        }
+      }
+    }, function(err, data) {
+      console.log('INVITED: ', user1.firstName+" invited "+user2.lastName+" to connect.", err, data);
+      callback();
+    });
+
+  },
+
+  afterCreate: function(connection, callback) {
+    var self = this;
     console.log(sails.config.linkedin);
     var c = sails.config.linkedin;
     var Linkedin = require('node-linkedin')(c.apiKey, c.apiSecret, c.callbackUri);
@@ -35,8 +64,6 @@ module.exports = {
         if (results.length == 1) {
           var user1 = results[0];
 
-          var myAccessToken = user1.linkedinAccessToken;
-          var linkedin = Linkedin.init(myAccessToken);
 
           sails.models.user
             .find()
@@ -47,27 +74,10 @@ module.exports = {
               if (results.length == 1) {
                 var user2 = results[0];
 
-                linkedin.people.invite({
-                  "recipients": {
-                    "values": [{
-                      "person": {
-                        "_path": "/people/email=" + user2.emailAddress,
-                        "first-name": user2.firstName,
-                        "last-name": user2.lastName
-                      }
-                    }]
-                  },
-                  "subject": "Invitation to connect.",
-                  "body": "We met with Mymi",
-                  "item-content": {
-                    "invitation-request": {
-                      "connect-type": "friend"
-                    }
-                  }
-                }, function(err, data) {
-                  console.log('INVITED?', err, data);
-                  callback();
+                self.sendInvite(user1, user2, function() {
+                  self.sendInvite(user2,user1,callback);
                 });
+
               } else {
                 console.log("did not find user2");
                 callback();
